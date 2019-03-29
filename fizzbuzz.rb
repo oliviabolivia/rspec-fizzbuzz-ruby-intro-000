89c1748244d60107092fd30d9c73497b4de22b3f
Olivia R <orquigley@gmail.com>
2019-03-29 22:50:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  elsif int % 3 == 0 
+  elsif int % 3 == 0
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  else 
+  else
     return nil
 end
